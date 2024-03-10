49a07969a240dda5ce159445a40930de94700e50
JuPillet <JuPillet.juju.pillet@hotmail.fr>
2024-03-10 20:47:42 +0100

    precision bonus wip

--- trainer.py
+++ trainer.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
 import sys
-#import matplotlib
-#matplotlib.use('TkAgg')
+import tkinter
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 def printRegressionLine(ax, kms, pcs, theta0, theta1):
@@ -14,8 +15,10 @@ def printRegressionLine(ax, kms, pcs, theta0, theta1):
 	ax.legend()
 	plt.pause(0.00001)
 
-def printModelLR(theta0, theta1):
-	print(f"theta0: {theta0}\ntheta1: {theta1}")
+def printModelLR(theta0, theta1, COD):
+	print(f"theta0: {theta0}")
+	print(f"theta1: {theta1}")
+	print(f"COD (Precision): {COD}")
 	with open('modelLR', 'w') as ModelLR:
 		ModelLR.write(f"theta0:{theta0}\n")
 		ModelLR.write(f"theta1:{theta1}")
@@ -23,17 +26,15 @@ def printModelLR(theta0, theta1):
 def estimatePrice(km, theta0, theta1):
 	return theta0 + theta1 * km
 
-def calculatePrecision(kms, m, meanPc, theta0, theta1):
-	meanEstimatedPcs = None
-	for km in kms:
-		meanEstimatedPcs += estimatePrice(km, theta0, theta1)
-	meanEstimatedPcs /= m
-	precision = (meanEstimatedPcs / meanPc if meanEstimatedPcs < meanPc else meanPc / meanEstimatedPcs) * 100
-	return precision
-	
-#def diverter(m, datas, average):
-#	nrmDatas = sum((i - average) ** 2 for i in datas)
-#	return (nrmDatas / m) ** 0.5
+def calculatePrecision(stdKms, stdPcs, m, stdTheta0, stdTheta1):
+	meanStdPcs = sum(stdPcs) / m
+#	Sum of Square's Residuals
+	SSR = sum([(estimatePrice(stdKm, stdTheta0, stdTheta0) - meanStdPcs) ** 2] for stdKm in stdKms)
+#	Sum of Square's total
+	SST = sum([(stdPc - meanStdPcs) ** 2] for stdPc in stdPcs)
+#	coefficient of determination
+	COD = 1 - (SSR/SST)
+	return COD
 
 def standardizer(kms, pcs):
 	meanKms, meanPcs = np.mean(kms), np.mean(pcs)
@@ -50,14 +51,14 @@ def destandardizer(theta0, theta1, minKm, maxKm, minPc, maxPc):
 	return tmpTheta0, tmpTheta1
 
 def training(m, stdKms, stdPcs, minKm, maxKm, minPc, maxPc, ax, kms, pcs):
-	learningRate = 0.001
-	iterrations = 100000
+	learningRate = 0.1
+	iterrations = 1000
 	midRate = learningRate / m
 
+	stdTheta0 = 0
+	stdTheta1 = 0
 	theta0 = 0
 	theta1 = 0
-	deTheta0 = 0
-	deTheta1 = 0
 #boucle principale d'itération d entrainement
 	for _ in range (iterrations):
 		tmp0 = 0
@@ -67,20 +68,20 @@ def training(m, stdKms, stdPcs, minKm, maxKm, minPc, maxPc, ax, kms, pcs):
 			km = stdKms[i]
 			pc = stdPcs[i]
 #calcul d'erreur
-			error = estimatePrice(km, theta0, theta1) - pc
+			error = estimatePrice(km, stdTheta0, stdTheta1) - pc
 #calcule temportaire de Theta0 et Theta1
 			tmp0 += error
 			tmp1 += (error * km)
 #ajustement Theta0 et Theta1
-		theta0 -= tmp0 * midRate
-		theta1 -= tmp1 * midRate
-		deTheta0, deTheta1 = destandardizer(theta0, theta1, minKm, maxKm, minPc, maxPc)
-		printRegressionLine(ax, kms, pcs, deTheta0, deTheta1)
-	return deTheta0, deTheta1
+		stdTheta0 -= tmp0 * midRate
+		stdTheta1 -= tmp1 * midRate
+		theta0, theta1 = destandardizer(stdTheta0, stdTheta1, minKm, maxKm, minPc, maxPc)
+		printRegressionLine(ax, kms, pcs, theta0, theta1)
+	return stdTheta0, stdTheta1, theta0, theta1
 
 def	main():
 	argc : int | str = len(sys.argv)
-	bonus = False if argc == 2 else sys.argv[1]
+	bonus = sys.argv[1] if argc == 2 else False
 	if bonus != False and bonus != "bonus":
 		raise Exception(f"option '{bonus}' invalide et non reconnu")
 	csv = pd.read_csv("./Subject/data.csv")
@@ -89,9 +90,9 @@ def	main():
 	m = len(kms)
 	meanKms, meanPcs, minKm, maxKm, minPc, maxPc, stdKms, stdPcs = standardizer(kms, pcs)
 	fig, ax = plt.subplots()
-	theta0, theta1 = training(m, stdKms, stdPcs, minKm, maxKm, minPc, maxPc, ax, kms, pcs)
-	precision = calculatePrecision(kms, m, meanKms, meanPcs, theta0, theta1)
-	printModelLR(theta0, theta1)
+	stdTheta0, stdTheta1, theta0, theta1 = training(m, stdKms, stdPcs, minKm, maxKm, minPc, maxPc, ax, kms, pcs)
+	COD = calculatePrecision(stdKms, stdPcs, m, stdTheta0, stdTheta1)
+	printModelLR(theta0, theta1, COD)
 	printRegressionLine(ax, kms, pcs, theta0, theta1)
 	plt.show()
 
@@ -104,5 +105,5 @@ if __name__ == "__main__":
 		print("Format CSV incorrect.")
 	except FileNotFoundError:
 		print("Le fichier CSV n'a pas été trouvé.")
-	except Exception as e:
-		print(f"Une erreur s'est produite : {e}")
+	#except Exception as e:
+	#	print(f"Une erreur s'est produite : {e}")
